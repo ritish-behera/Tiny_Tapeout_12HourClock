a701d0f930740cac39b34db1522ceeb1b3d056da
ritish-behera <158822580+ritish-behera@users.noreply.github.com>
2024-06-15 22:25:15 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -33,11 +33,9 @@ module tt_um_ritish_behera_digitalClock (
 
     
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
